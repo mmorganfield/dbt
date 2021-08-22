034e9e7e4b90571ecb793d46973abfc2f1a5c13f
maxwell.morganfield@gmail.com <maxwell.morganfield@gmail.com>
2021-08-22 20:07:54 +0000

    updating for testing new pattern: manual base

--- models/staging/google_calendar/stg_google_calendar.sql
+++ models/staging/google_calendar/stg_google_calendar.sql
@@ -1,6 +1,6 @@
-WITH base_google_calendar AS
+WITH stg_google_calendar AS
 (
     SELECT * FROM {{ source('dev_base', 'base_google_calendar') }}
 )
 
-SELECT * FROM base_google_calendar
+SELECT * FROM stg_google_calendar
