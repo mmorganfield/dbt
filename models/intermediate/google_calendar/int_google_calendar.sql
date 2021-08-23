1e6e8180aba91b4afdf94f5722b234e52825e67b
maxwell.morganfield@gmail.com <maxwell.morganfield@gmail.com>
2021-08-23 00:32:58 +0000

    fixed int_google_calendar

--- models/intermediate/google_calendar/int_google_calendar.sql
+++ models/intermediate/google_calendar/int_google_calendar.sql
@@ -11,4 +11,32 @@ stg_google_calendar_dates AS (
 
 SELECT 
 
-* FROM stg_google_calendar_dates
+    CAST(FORMAT_TIMESTAMP("%Y%m%d", start_time) AS INT64 ) AS date_key
+    ,location
+    ,description
+    ,event_recurrence
+    ,event_timezone
+    ,end_time
+    ,DATE(end_time) AS end_date
+    ,start_time
+    ,DATE(start_time) AS start_date
+    ,event_attendees_name
+    ,event_attendees_email
+    ,event_attendees_response_status
+    ,event_summary
+    ,event_updated_time
+    ,event_created_time
+    ,organizer_name
+    ,CASE 
+        WHEN is_organizer_self
+            THEN 1
+        WHEN is_organizer_self IS NULL 
+            THEN 0
+        ELSE 0
+        END
+        AS is_organizer_self_ind
+    ,organizer_email
+    ,_record_create_dt
+    
+
+FROM stg_google_calendar_dates
