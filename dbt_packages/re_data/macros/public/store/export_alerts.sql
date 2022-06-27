d00e6b4dfba743bacab7d730954be54f229ca193
Maxwell Morganfield <maxwell.morganfield@gmail.com>
2022-06-27 16:30:58 -0600

    updated package contents

--- dbt_packages/re_data/macros/public/store/export_alerts.sql
+++ dbt_packages/re_data/macros/public/store/export_alerts.sql
@@ -1,4 +1,4 @@
-{% macro export_alerts(start_date, end_date, alerts_path=None) %}
+{% macro export_alerts(start_date, end_date, alerts_path=None, monitored_path=None) %}
     {% set alerts_query %}
         select
             type as {{ re_data.quote_column('type') }},
@@ -10,7 +10,7 @@
         where
             case
                 when type = 'anomaly' then time_window_end between '{{ start_date }}' and '{{ end_date }}'
-                else true
+                else time_window_end >= '{{ start_date }}'
             end
         order by time_window_end desc
     {% endset %}
@@ -18,4 +18,5 @@
     {% set query_result = run_query(alerts_query) %}
     {% set alerts_file_path = alerts_path or 'target/re_data/alerts.json' %}
     {% do query_result.to_json(alerts_file_path) %}
+    {{ save_monitored(monitored_path) }}
 {% endmacro %}
