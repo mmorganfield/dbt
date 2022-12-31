79e90f7115b2eb7f96424b4d318611680fd84294
Maxwell Morganfield <maxwell.morganfield@gmail.com>
2022-12-31 16:39:59 -0700

    updated table reference names

--- models/prod_dw/covid/county_cases.sql
+++ models/prod_dw/covid/county_cases.sql
@@ -1,5 +1,5 @@
-WITH int_county_cases AS (
+WITH stg_county_cases AS (
     SELECT * FROM {{ ref( 'stg_county_cases') }}
 )
 
-SELECT DISTINCT * FROM int_county_cases
+SELECT DISTINCT * FROM stg_county_cases
